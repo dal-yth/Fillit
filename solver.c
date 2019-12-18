8bc0af70a12b6e50a7868ea77b3ce20377255436
Jani Mäkelä <58624979+jmakela42@users.noreply.github.com>
2019-12-18 23:13:13 +0200

    Shortened remove piece

--- solver.c
+++ solver.c
@@ -14,41 +14,21 @@
 
 /*
 ** Function takes as a parameter the field, the block that needs to be
-** removed and the field size. It returns TRUE (1) on succesful removal
-** The function goes through the block until it finds the ascii character
-** used to mark the block. It then goes through the field and chances all
-** blocks with that character to '.'.
+** removed and the field size. It returns TRUE (1) on succesful removal.
+** It then goes through the field and chances all blocks with matching
+** tmins->c character to dots.
 */
-int		remove_piece(char ***field, t_list *tmins, int field_size)
+static int	remove_piece(char ***field, t_list *tmins, int field_size)
 {
-	int		i;
-	int		w;
-	char	c;
 	t_point	point;
 
-	i = 0;
-	w = 0;
 	point.x = 0;
 	point.y = 0;
-	while (w < 4)
-	{
-		while(i < 4)
-		{
-			if (tmins->data[w][i] != '.')
-			{
-				c = tmins->data[w][i];
-				break;
-			}
-			i ++;
-		}
-		i = 0;
-		w++;
-	}
-	if (field[0][point.y][point.x] == c)
+	if (field[0][point.y][point.x] == tmins->c)
 		field[0][point.y][point.x] = '.';
 	while (add_point(&point, field_size))
 	{
-		if (field[0][point.y][point.x] == c)
+		if (field[0][point.y][point.x] == tmins->c)
 			field[0][point.y][point.x] = '.';
 	}
 	return (TRUE);
@@ -60,13 +40,13 @@ int		remove_piece(char ***field, t_list *tmins, int field_size)
 ** able to set the piece, and FALSE (0), if it could not set the piece
 ** First the function goes through the piece to find the first block cell. This
 ** point is saved in struct start, which contains the x and y coordinates. Then
-** the function goes through the rest of the cells of the piece and sets every 
-** block cell to the field. If the function tries to write outside field or has 
-** added some other amount of block cells than 4, it will remove the ones it has 
+** the function goes through the rest of the cells of the piece and sets every
+** block cell to the field. If the function tries to write outside field or has
+** added some other amount of block cells than 4, it will remove the ones it has
 ** added and return FALSE.
 */
 
-int		set_piece(char ***field,t_list *tmins,int field_size, t_point *point)
+static int	set_piece(char ***field, t_list *tmins, int field_size, t_point *point)
 {
 	t_point	piece;
 	t_point	map;
@@ -115,18 +95,18 @@ int		set_piece(char ***field,t_list *tmins,int field_size, t_point *point)
 ** THE RECURSIVE SOLVER! *drum roll*
 ** Takes as a parameter the field, list of blocks (tmins), size of the field
 ** (the number of blocks on one side of the square) and a point that contains
-** values for x and y on the field (set to 0, 0 when first called)
+** values for x and y on the field (set to 0, 0 when first called).
 ** First, finds and empty point where no block has been set. Then tries to set
-** the next block to it. If this fails, finds the next empty point. If there is 
+** the next block to it. If this fails, finds the next empty point. If there is
 ** no next empty point, returns false (meaning the field could not be solved this
-** way). If all the blocks has been set (tmins-> next == NULL), returns true,
-** meaning the rigth answer has been found. When the piece has has been set, calls
-** on itself with the next piece. If this operation returns true, the function 
+** way). If all the blocks has been set (tmins == NULL), returns true,
+** meaning the right answer has been found. When the piece has has been set, calls
+** on itself with the next piece. If this operation returns true, the function
 ** continues to return true. In other cases removes the piece set and goes to the
-** next point. Continues then at the beginning of while.
+** next point. Continues then at the beginning of while loop.
 */
 
-int		recursive_solver(char ***field, t_list *tmins, int field_size)
+static int	recursive_solver(char ***field, t_list *tmins, int field_size)
 {
 	t_point point;
 
@@ -136,12 +116,12 @@ int		recursive_solver(char ***field, t_list *tmins, int field_size)
 		return (TRUE);
 	while (find_empty_point(field, &point, field_size))
 	{	
-		while(!(set_piece(field, tmins, field_size, &point)))
+		while (!(set_piece(field, tmins, field_size, &point)))
 		{
 			if (!(add_point(&point, field_size)) || !(find_empty_point(field, &point, field_size)))
 				return (FALSE);
 		}
-		if(!(recursive_solver(field, tmins->next, field_size)))
+		if (!(recursive_solver(field, tmins->next, field_size)))
 		{
 			remove_piece(field, tmins, field_size);
 			if (!(add_point(&point, field_size)))
@@ -154,10 +134,10 @@ int		recursive_solver(char ***field, t_list *tmins, int field_size)
 }
 
 /*
-** First makes a 3x3 field, and uses recursive solver to solve it.
-** if recursive solver fails, makes the field one bigger, and does 
-** this in a loop until the solution is found. Writes the new field
-** straight in to the parameter and returns the size of the field
+** Makes smallest possible field that can fit all the pieces.
+** Then attempts to solve the field, if recursive solver fails,
+** makes the field one bigger, and does this in a loop until
+** the solution is found.
 */
 
 void		solver(t_list *tmins, char ***field, int pieces)
