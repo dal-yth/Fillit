11969960312207e5ca3e08773d39e508eddfc01d
Jani Mäkelä <58624979+jmakela42@users.noreply.github.com>
2019-12-18 23:09:28 +0200

    Added finding char c

--- list.c
+++ list.c
@@ -13,12 +13,14 @@
 #include "fillit.h"
 
 /*
-** Mod_buf korvaa hashtagit oikeilla kirjaimilla ja palauttaa muokatun buffin.
+** Mod_buf finds hashtags in the string buf and changes them to
+** char c, which is something between A-Z depending on which
+** tetrimino we are currently saving to the list.
 */
 
-char *mod_buf(char *buf, char c)
+static char		*mod_buf(char *buf, char c)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while(buf[i])
@@ -31,84 +33,119 @@ char *mod_buf(char *buf, char c)
 }
 
 /*
-** Kiitos Jaakko.
-** Tässä luodaan uusi node, jonka char **data mallocoidaan,
-** jotta jokaiselle saadaan oma erillinen muistipaikka,
-** eikä pointteri yritä hakea aina samasta muistipaikasta arvoja.
+** Finds which character the current tetrimino has and returns it.
+** The char is then saved in newnode function to the new member of the list.
 */
 
-t_list *newnode(char **data)
+static char		find_char(char **data)
 {
-	t_list *new;
-	int i;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (data[i])
+	{
+		while (data[i][j])
+		{
+			if (data[i][j] != '.')
+				return (data[i][j]);
+			j = j + 1;
+		}
+		j = 0;
+		i = i + 1;
+	}
+	return (0);
+}
+
+/*
+** Thank you Jaakko.
+** Newnode takes two dimensional string as data and stores
+** it to our list. New node and it's data is allocated each
+** time, because without allocation, new->data always points to
+** a memory location of char **data and when save_list function
+** assigns different data to char **data, it changes data within
+** the list as well.
+*/
+
+static t_list	*newnode(char **data)
+{
+	t_list	*new;
+	int		i;
 
     i = 0;
     new = (t_list*)malloc(sizeof(t_list));
     new->data = (char**)malloc(sizeof(char*) * 5);
     while (i <= 3)
     {
-        //new->data[i] = (char*)malloc(sizeof(char) * 5);
         new->data[i] = data[i];
         i = i + 1;
     }
 	new->data[i] = NULL;
 	new->next = NULL;
-	return(new);
+	new->c = find_char(new->data);
+	return (new);
 }
 
 /*
-** Lisää listan loppuun uuden noden, joka saa char **datan.
+** Appends a new node at the end of the list.
+** Gets the pointer to the head of the list and data to
+** assign to the new node. We create temporary node last
+** which becomes the new temporary head. We can then go through
+** the list, until we find the last node, then we assign
+** a new node to be the last node. Returns i + 1,
+** which is used to count how many tetriminoes we have.
 */
 
-void append(t_list **tetris, char **data)
+static int		append(t_list **tmins, char **data, int i)
 {
 	t_list *last;
 
-	last = *tetris;
-	if (*tetris == NULL)
+	last = *tmins;
+	if (*tmins == NULL)
 	{
-		*tetris = newnode(data);
-		return;
+		*tmins = newnode(data);
+		return (i + 1);
 	}
-	while(last->next != NULL)
+	while (last->next != NULL)
 		last = last->next;
 	last->next = newnode(data);
-	return;
+	return (i + 1);
 }
 
 /*
-** Sama funktio kuin save tetri, jota käytetään palojen tallentamiseen.
-** Muokattu toimimaan linked listin kanssa.
+** Saves the tetriminos to a linked list.
+** We use get_next_line to read argv data line by line.
+** Mod_buf changes hashtags into chars A-Z.
+** Those lines are saved to a two dimensional str piece.
+** Piece is then saved as data to the list. Returns the
+** amount of pieces saved to list.
 */
 
-int save_list(t_list **tetris, char **argv)
+int				save_list(t_list **tmins, char **argv)
 {
-	int fd;
-	char *buf;
-	char **piece;
-	int i;
-	int j;
+	int		fd;
+	char	*buf;
+	char	**piece;
+	int		i;
+	int		j;
 
 	i = 0;
 	j = 0;
-	piece = (char**)malloc(sizeof(char*) * 5);
+	piece = (char**)malloc(sizeof(char*) * 6);
 	fd = open(argv[1], O_RDONLY);
 	while ((get_next_line(fd, &buf)) > 0)
 	{
 		buf = mod_buf(buf, 'A' + i);
 		if (j <= 3)
-			piece[j] = ft_strdup(buf);
+			piece[j] = buf;
 		if (j == 3)
-			append(tetris, piece);
+			i = append(tmins, piece, i);
 		j = j + 1;
 		if (j == 5)
-		{
-			i = i + 1;
 			j = 0;
-		}
-		free(buf);
 	}
 	close(fd);
 	free(piece);
-	return(i + 1);
+	return (i);
 }
