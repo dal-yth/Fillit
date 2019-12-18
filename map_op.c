d39ac0afe9209e032fb24351af6042a190433277
Jani Mäkelä <58624979+jmakela42@users.noreply.github.com>
2019-12-18 23:11:59 +0200

    Free field function added

--- map_op.c
+++ map_op.c
@@ -2,10 +2,10 @@
 
 /*
 ** The function creates a new string of size num, and allocates it with
-** value. The fresh string is returned.
+** char value. The fresh string is then returned.
 */
 
-char	*ft_strset(char value, size_t num)
+static char	*ft_strset(char value, size_t num)
 {
 	unsigned int	i;
 	char			*returnable;
@@ -24,10 +24,11 @@ char	*ft_strset(char value, size_t num)
 }
 
 /*
-** Printer funktion for the field;
+** Printer function for the field.
+** It prints the field. That's it.
 */
 
-void print_field(char ***field)
+void		print_field(char ***field)
 {
 	int i;
 
@@ -41,35 +42,52 @@ void print_field(char ***field)
 }
 
 /*
-** New make_field 
+** Frees the allocated field field.
 */
 
-int		make_field(char ***field, int pieces, int grow)
+static void	free_field(char ***field)
+{
+	int i;
+
+	i = 0;
+	while (field[0][i] != NULL)
+	{
+		free(field[0][i]);
+		i++;
+	}
+	free(field[0]);
+}
+
+/*
+** Function to make the field for tetriminos.
+** Gets the pointer to a two dimensional string field,
+** amount of tetriminoes and grow, which is used to make
+** bigger fields if first one can't fit all tetriminoes.
+** Check for grow if we have to free the old field, before
+** creating a new field. Then checks based on piece count,
+** how large field it should create and allocates memory
+** for it. Ft_strset fills the field with dots.
+*/
+
+int			make_field(char ***field, int pieces, int grow)
 {
 	int		i;
 	int		size;
+
 	size = 2;
 	i = 0;
-	if(grow > 0)
-	{
-		while (field[0][i] != NULL)
-		{
-			free(field[0][i]);
-			i++;	
-		}
-		free(field);
-	}
+	if (grow > 0)
+		free_field(field);
 	while (size * size < (pieces * 4))
 		size = size + 1;
 	size = size + grow;
 	*field = (char**)malloc(sizeof(char*) * (size + 1));
 	i = 0;
-	while(i < size)
+	while (i < size)
 	{
 		field[0][i] = ft_strset('.', size);
 		i = i + 1;
 	}
 	field[0][i] = NULL;
-	return(size);
+	return (size);
 }
-
