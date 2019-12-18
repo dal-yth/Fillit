3db13acb77febbb0c1eee4c0803f4eb75afd1d75
Jani Mäkelä <58624979+jmakela42@users.noreply.github.com>
2019-12-18 23:10:43 +0200

    Slight clean up

--- main.c
+++ main.c
@@ -1,36 +1,27 @@
 #include "fillit.h"
 
 /*
-** Ajatuksena on että main kutsuu vuorotellen ensin
-** validointifunktion, joka kutsuu kaikki muut inputin tarkistukset,
-** sitten tallennusfunktion, joka kutsuu funktiot tunnistamaan ja
-** tallentamaan tetriminoja jonnekin ja viimeisenä solverin, joka
-** kutsuu kaikki ratkaisufunktiot.
-** -------------------------------------------------------------------
-** Tällä hetkellä koodi on tietysti aika sekametelisoppaa ja kaikki on
-** yhdessä tiedostossa, sen lisäksi luultavasti jotkin funktiot ovat
-** liian pitkiä ja norminettestä en halua edes tietää :D mutta jos
-** systeemin saisi jotenkin rakennettua ensin kasaan ja sitten voisi
-** hienosäätää ja siistiä.
-** -------------------------------------------------------------------
-** Mainissa on edelliskerrasta printtilooppeja testaamista varten ja 
-** paljon turhia muuttujia. Tällä hetkellä printtaa tallennetut
-** tetriminot, joihin on laitettu oikeat merkit A-Z riippuen tetriminojen
-** määrästä. 
-*/
-
-/*
-** funktio joka näyttää usagen
+** Shows the usage of fillit.
 */
 
 void	put_usage_message()
 {
-	ft_putendl("usage: fillit input_file");
-	ft_putendl("    input_file: File containing tertimino pieces. Inputfile should follow these rules:");
+	ft_putendl("Usage: fillit input_file");
+	ft_putendl("    input_file: File containing tetrimino pieces. Input file should follow these rules:");
 	ft_putendl("    Each tetrimino is precisely 4 lines of 4 characters, each followed by a new line");
-	ft_putendl("    Each character must be either a block character(’#’ ) or an empty character (’.’)");
+	ft_putendl("    Each character must be either a block character('#') or an empty character ('.')");
 	ft_putendl("    Your file should contain between 1 and 26 Tetriminos.");
 }
+
+/*
+** Main, where we do the error checking for invalid amount of arguments.
+** If argument count is invalid, we put usage message. Else, we send the
+** argument for validation. Wrong files, wrong format, wrong tetriminoes etc.
+** result in "invalid input". If everything is OK, save_list gets the file
+** and uses it to count the pieces and make a linked list where pieces are
+** saved. Next the list is sent to the solver, which creates the map and
+** does the actual solving of the puzzle. Finally we print the solution.
+*/
 
 int		main(int argc, char **argv)
 {
@@ -42,17 +33,15 @@ int		main(int argc, char **argv)
 	if (argc != 2)
 	{
 		put_usage_message();
-		return(0);
+		return (0);
 	}
 	if (!(validate_input(argv)))
 	{
 		write(1, "invalid input", 13);
-		return(0);
+		return (0);
 	}
 	pieces = save_list(&tmins, argv);
-	printf("%d\n", pieces);
-	ft_putendl("all ok");
 	solver(tmins, &field, pieces);
 	print_field(&field);
-	return(0);
+	return (0);
 }
